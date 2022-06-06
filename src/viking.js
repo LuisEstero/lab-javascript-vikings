cefecd6e93e34338d6d2f85d18db7ba5d52730e2
LuisEstero <104272882+LuisEstero@users.noreply.github.com>
2022-06-06 21:58:18 +0200

    The Soldier is dead

--- src/viking.js
+++ src/viking.js
@@ -15,14 +15,64 @@ class Soldier {
 
 
 // Viking
-class Viking {
-
+class Viking extends Soldier {
+constructor(name, health, strength){ 
+  this.name = name;
+  super(health,strength) ;
 }
+attack (){
+  return this.strength;
+}
+receiveDamage(damage){
+  this.health -=health;
+  if(this.health <= 0 ) {
+    console.log( NAME +  "has received DAMAGE points of damage")
+  } 
+    return (NAME +  "has died in act of combat")
+  }
+
+  }
+  
+
+
 
 // Saxon
-class Saxon {
+class Saxon extends Soldier {
 
+  attack (){
+    this.strength = this.strength
+  }
+  receiveDamage (damage){
+    this.health -=health;
+    if(saxon >= 0){
+      return 'A Saxon has received DAMAGE points of damage';
+    } else {
+       return "A Saxon has died in combat";
+
+    }
+  }
 }
+
+
+
+
+// Saxon
+class Saxon extends Soldier {
+
+  attack (){
+    this.strength = this.strength
+  }
+  receiveDamage (damage){
+    this.health -=health;
+    if(saxon >= 0){
+      return 'A Saxon has received DAMAGE points of damage';
+    } else {
+       return "A Saxon has died in combat";
+
+    }
+  }
+}
+
 
 // War
 class War {
